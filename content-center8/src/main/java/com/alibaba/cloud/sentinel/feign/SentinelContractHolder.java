677c792e13de9f0af6e4f86095ab95c2c5dad931
524991368@qq.com <5xuelabi>
2020-11-15 19:55:39 +0800

    分章节创建模块

--- content-center8/src/main/java/com/alibaba/cloud/sentinel/feign/SentinelContractHolder.java
+++ content-center8/src/main/java/com/alibaba/cloud/sentinel/feign/SentinelContractHolder.java
@@ -1,34 +1,34 @@
-package com.alibaba.cloud.sentinel.feign;
-
-import feign.Contract;
-import feign.MethodMetadata;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-/**
- * 目前Sentinel和Feign整合的bug，下个版本就没了
- */
-public class SentinelContractHolder implements Contract {
-
-    private final Contract delegate;
-
-    /**
-     * map key is constructed by ClassFullName + configKey. configKey is constructed by
-     * {@link feign.Feign#configKey}
-     */
-    public final static Map<String, MethodMetadata> METADATA_MAP = new HashMap<>();
-
-    public SentinelContractHolder(Contract delegate) {
-        this.delegate = delegate;
-    }
-
-    @Override
-    public List<MethodMetadata> parseAndValidateMetadata(Class<?> targetType) {
-        List<MethodMetadata> metadatas = delegate.parseAndValidateMetadata(targetType);
-        metadatas.forEach(metadata -> METADATA_MAP
-                .put(targetType.getName() + metadata.configKey(), metadata));
-        return metadatas;
-    }
-}
+//package com.alibaba.cloud.sentinel.feign;
+//
+//import feign.Contract;
+//import feign.MethodMetadata;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * 目前Sentinel和Feign整合的bug，下个版本就没了
+// */
+//public class SentinelContractHolder implements Contract {
+//
+//    private final Contract delegate;
+//
+//    /**
+//     * map key is constructed by ClassFullName + configKey. configKey is constructed by
+//     * {@link feign.Feign#configKey}
+//     */
+//    public final static Map<String, MethodMetadata> METADATA_MAP = new HashMap<>();
+//
+//    public SentinelContractHolder(Contract delegate) {
+//        this.delegate = delegate;
+//    }
+//
+//    @Override
+//    public List<MethodMetadata> parseAndValidateMetadata(Class<?> targetType) {
+//        List<MethodMetadata> metadatas = delegate.parseAndValidateMetadata(targetType);
+//        metadatas.forEach(metadata -> METADATA_MAP
+//                .put(targetType.getName() + metadata.configKey(), metadata));
+//        return metadatas;
+//    }
+//}
